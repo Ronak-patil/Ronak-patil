fbb155cfdb3e280293575c3db65a4a28a2849261
Ronak-patil <89696600+Ronak-patil@users.noreply.github.com>
2021-08-29 16:57:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,103 @@
-const Engine=Matter.Engine
-const World=Matter.World
-const Bodies=Matter.Bodies
+//name spacing
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine,world
+// Only for debugging code, is this needed, not otherwise
+// const Render = Matter.Render;
 
-function setup() {
-  createCanvas(800,400);
-  engine=Engine.create()
-  world=engine.world
-  var ground_options={isStatic:true}
-  ground=Bodies.rectangle(400,390,800,20,ground_options)
-  World.add(world,ground)
-  var ball_options={restitution:1}
-  ball=Bodies.circle(400,100,60,ball_options)
-  World.add(world,ball)
+var myengine,myworld;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(800,400);
+   
+    myengine = Engine.create();
+    myworld = myengine.world;
+
+    //Code for brown ground
+    var ground_options ={
+        isStatic: true,
+      
+    }
+    ground = Bodies.rectangle(400,390,800,20,ground_options);
+    World.add(myworld,ground);
+    console.log(ground);
+
+    // Code for red box
+    var redBox_options ={
+        isStatic: true,
+    }
+
+    redBox = Bodies.rectangle(290,320,100,120,redBox_options);
+    World.add(myworld,redBox);
+
+// Code for green box
+var greenBox_options ={
+    isStatic: true,
 }
 
-function draw() {
-  background(0);  
-  drawSprites();
-  Engine.update(engine)
-  rectMode(CENTER)
-  fill('yellow')
-  rect(ground.position.x,ground.position.y,800,20)
-  fill('red')
-  ellipseMode(RADIUS)
-  ellipse(ball.position.x,ball.position.y,60,60)
+greenBox = Bodies.rectangle(400,320,100,120,greenBox_options);
+World.add(myworld,greenBox);
+
+// Code for yellow box
+var yellowBox_options ={
+    isStatic: true,
+    angle:-60
+    
+}
+
+yellowBox = Bodies.rectangle(600,320,100,20,yellowBox_options);
+// Matter.Body.setAngle(yellowBox,130)
+World.add(myworld,yellowBox);
+    
+    // Code for ball
+    var ball_options ={
+        isStatic: true,
+        restitution: 0
+    }
+    var ball_options={restitution:1.3}
+    ball = Bodies.circle(612,100,20, ball_options);
+    World.add(myworld,ball);
+    
+
+    // Only for debugging code, is this needed, not otherwise
+    // var render = Render.create({
+    //   element: document.body,
+    //   engine: myengine,
+    //   options: {
+    //     width: 1600,
+    //     height: 700,
+    //     wireframes: false
+    //   }
+    // });
+    // Render.run(render);
+
+    //console.log(ball);
+}
+
+function draw(){
+    background("lightgreen");
+    Engine.update(myengine);
+    rectMode(CENTER);
+
+    fill("brown")
+    rect(ground.position.x,ground.position.y,800,20);
+    fill("red")
+    rect(redBox.position.x,redBox.position.y,100,120);
+    fill("green")
+    rect(greenBox.position.x,greenBox.position.y,100,120);
+    
+    fill("yellow")
+    push();
+    translate(yellowBox.position.x,yellowBox.position.y);
+    rotate(-45);
+    rectMode(CENTER);
+    rect(0, 0, 100, 20);
+    pop();
+   
+    fill("red")
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
 }
